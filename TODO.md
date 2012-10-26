72ba96aaa6f74e69d964b98fbe66eceba11cfb4a
Lars Fronius <l.fronius@googlemail.com>
2012-10-26 14:50:51 +0200

    first commit

---
+++ TODO.md
@@ -0,0 +1,7 @@
+ * fetch ec2-metadata service and provide variables in python template for de-/provisioning
+ * include python templates for de-/provisioning tasks
+ * debianize (setup.py/fpm/classic debian-folder)
+ * init.d
+ * README.md
+ * command-line-argument aware config parser
+ * configurable log level
---
+++ worker.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import boto
+import json
+import logging
+import sys
+import pprint
+import re
+import ConfigParser
+
+config = ConfigParser.ConfigParser()
+config.read('worker.cfg')
+
+logger = logging.getLogger('squarepulse')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+try:
+    AWSAccessKeyId = config.get('squarepulse','awsaccesskeyid')
+    AWSSecretKey = config.get('squarepulse','awssecretkey')
+    SQSQueue = config.get('squarepulse','sqsqueue')
+except NoOptionError as e:
+    logger.error(e)
+    sys.exit(1)
+
+sqs = boto.connect_sqs(aws_access_key_id=AWSAccessKeyId,aws_secret_access_key=AWSSecretKey)
+ec2 = boto.connect_ec2(aws_access_key_id=AWSAccessKeyId,aws_secret_access_key=AWSSecretKey)
+
+if "region" in dict(config.items('squarepulse')):
+    sqs = boto.sqs.connect_to_region(config.get('squarepulse','region'))
+    ec2 = boto.ec2.connect_to_region(config.get('squarepulse','region'))
+
+q = sqs.get_queue(SQSQueue)
+
+q.set_message_class(boto.sqs.message.RawMessage)
+
+def extract_message(rawMessage):
+    body = json.loads(rawMessage.get_body())
+    kv = [e.split('=', 1) for e in body['Message'].splitlines()]
+    kv = [(k, v.strip("'")) for k,v in kv]
+    message = dict(kv)
+    return message
+
+
+while True:
+    try:
+        rawMessage = q.read()
+        if rawMessage is not None:
+            message = extract_message(rawMessage)
+            if any(message['ResourceStatus'] in s for s in config.sections()):
+                if message['ResourceType'].lower().replace('::','_') in dict(config.items('squarepulse.' + message['ResourceStatus'])):
+                    reservations = ec2.get_all_instances(instance_ids=[message['PhysicalResourceId']])
+                    instance = reservations[0].instances[0]
+                    print(instance.private_dns_name)
+
+    except KeyboardInterrupt:
+        sys.exit(0)
+
